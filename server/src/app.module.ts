c75a0f0669a2c31c035dbd912c0425204bdab195
Alexander Wiebe <alexanderjwiebe@gmail.com>
2024-04-26 22:49:26 -0600

    updated the tsconfigs  to work together

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/browser'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(__dirname);
+  }
+}
